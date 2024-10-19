be5116619b7f24be05622b5785850487c2700849
designermanjeets@gmail.com <designermanjeets@gmail.com>
2024-10-19 15:12:49 +0530

    SendEmailer

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const route = express.Router();
+const port = process.env.PORT || 8080;
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    type: "SMTP",
+    host: "smtp.gmail.com",
+    secure: true,
+    auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASS
+    }
+});
+
+app.use('/v1', route);
+
+route.post('/send-email', (req, res) => {
+    const { recipient, subject, text } = req.body;
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: recipient,
+        subject: subject,
+        text: text,
+        html: "<b>Hello world?</b>", // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send('Error sending email: ' + error);
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
